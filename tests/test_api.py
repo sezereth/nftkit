7dc8006378545880efe2211db18e8f9168e21b4c
sezereth <139017320+sezereth@users.noreply.github.com>
2024-02-26 21:09:21 +0300

    Create test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,7 @@
+import unittest
+from nftkit.api import NFTAPI
+
+class TestNFTAPI(unittest.TestCase):
+    def test_fetch_marketplace_data(self):
+        # Implement test for fetch_marketplace_data
+        pass
